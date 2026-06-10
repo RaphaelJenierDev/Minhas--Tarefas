fabfc9719c8335f2e2deea2c7f5f1f5eea375d17
Raphael Jeniêr <81784727+RaphaelJenierDev@users.noreply.github.com>
2026-06-10 11:06:28 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,35 +4,54 @@ const listaCompleta = document.querySelector('.list-tasks');
 
 let minhaListaDeItens = [];
 
+// Função que adiciona na memória e atualiza a tela
 function adicionarNovaTarefa() {
-    if (input.value.trim() === "") return;
+    if (input.value.trim() === '') return; // Não adiciona vazio
     
-    minhaListaDeItens.push(input.value);
-    input.value = "";
+    minhaListaDeItens.push({
+        tarefa: input.value,
+        concluida: false
+    });
+    
+    input.value = ''; // Limpa o campo
     mostrarTarefas();
 }
 
+// Função que desenha as tarefas na tela (o coração do projeto)
 function mostrarTarefas() {
-    let novaLi = "";
-    
-    minhaListaDeItens.forEach((item) => {
+    let novaLi = '';
+
+    minhaListaDeItens.forEach((item, posicao) => {
         novaLi += `
-            <li class="task">
-                <img src="img/checked.png" alt="check-na-tarefa">
-                <p>${item}</p>
-                <img src="img/trash.png" alt="deletar-tarefa">
+            <li class="task ${item.concluida ? 'done' : ''}">
+                <img src="./img/checked.png" onclick="concluirTarefa(${posicao})">
+                <p>${item.tarefa}</p>
+                <img src="./img/trash.png" onclick="deletarItem(${posicao})">
             </li>
         `;
     });
-    
+
     listaCompleta.innerHTML = novaLi;
 }
 
+// Função para marcar como concluída
+function concluirTarefa(posicao) {
+    minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida;
+    mostrarTarefas();
+}
+
+// Função para deletar
+function deletarItem(posicao) {
+    minhaListaDeItens.splice(posicao, 1);
+    mostrarTarefas();
+}
+
+// Evento do clique no botão
 button.addEventListener("click", adicionarNovaTarefa);
 
-// Evento para o botão Enter
-input.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
+// Evento do Enter no teclado
+input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
         adicionarNovaTarefa();
     }
 });
